d7119135e5b01355f4fa343242fdb6240884c3f4
kakon-aiubcse <kakon.aiubcse@gmail.com>
2024-10-31 05:52:28 +0600

    set up done for qr connect

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+export default function Home() {
+  return (
+    <>
+      <div
+        className="bg-teal-500 w-[600px] h-[200px] text-slate-50 text-[30px] flex relative font-manrope
+      text-center items-center pl-10 font-[700] left-[400px]"
+      >
+        Welcome to qr code generator app
+      </div>
+    </>
+  );
+}
