03de25d48be538465d88041d0e48916163beb278
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-07-13 14:45:49 +0600

    added editprofile ui

--- src/pages/dashboard/editprofile.js
+++ src/pages/dashboard/editprofile.js
@@ -6,7 +6,15 @@ const Editprofile = () => {
     <>
     <Sidebar/>
     <div className='ml-[16.666667%] xs:ml-0 xs:relative xs:top-[110px]'>
-      hellow?
+       <div className="flex flex-col items-center justify-center p-2 m-2">
+          <span className="flex items-center justify-center text-brand font-cp text-3xl">
+          Edit Profile
+          </span>
+         <div>
+          <span>
+             
+          </span>
+          </div></div>
     </div>
     </>
   )
