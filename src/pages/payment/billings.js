95f5bdc601711409809d5de92b77f5b4683da448
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-07-13 13:25:11 +0600

    payment ui added

--- src/pages/payment/billings.js
+++ src/pages/payment/billings.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import Sidebar from '../dashboard/sidebar';
+import React from "react";
+import Sidebar from "../dashboard/sidebar";
 
 const Billings = () => {
   return (
-       <>
-    <Sidebar/>
-    <div className='ml-[16.666667%] xs:ml-0 xs:relative xs:top-[110px]'>
-      hellow?
-    </div>
+    <>
+      <Sidebar />
+      <div className="ml-[16.666667%] xs:ml-0 xs:relative xs:top-[110px]">
+        <div className="flex flex-col items-center justify-center p-2 m-2">
+          <span className="flex items-center justify-center text-brand font-cp text-3xl">
+           Payment History
+          </span>
+          <div>
+            <span>All your payment details will display here:</span>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Billings;
