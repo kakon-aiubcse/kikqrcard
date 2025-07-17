3449579b442734cbfd0108293b3f467da14e505f
kakon-aiubcse <kakon.aiubcse@gmail.com>
2025-07-18 02:50:28 +0600

    updated logo

--- src/pages/components/logo.js
+++ src/pages/components/logo.js
@@ -12,34 +12,36 @@ const Logo = () => {
         xmlns="http://www.w3.org/2000/svg"
         className="flex relative right-3"
       >
-        <rect width="200" height="60" rx="12" ry="12" fill="transparent" />
+   <rect width="200" height="80" rx="12" ry="12" fill="#8F87F1" />
+  <text
+    x="30"
+    y="50"
+    fontFamily="Segoe UI, Tahoma, Geneva, Verdana, sans-serif"
+    fontSize="44"
+    fontWeight="700"
+    fill="white"
+  >
+    KIK
+  </text>
 
-        <text
-          x="25"
-          y="45"
-          fontFamily="Segoe UI, Tahoma, Geneva, Verdana, sans-serif"
-          fontSize={36}
-          fontWeight="700"
-          fill="white"
-        >
-          KIK
-        </text>
+ 
+  <circle cx="130" cy="20" r="5" fill="teal" />
+  <rect x="145" y="15" width="10" height="10" fill="blue" />
+  <rect x="145" y="35" width="5" height="5" fill="black" />
+  <circle cx="175" cy="40" r="3" fill="red" />
 
-        <circle cx="140" cy="20" r="5" fill="teal" />
-        <rect x="160" y="15" width="10" height="10" fill="blue" />
-        <rect x="160" y="35" width="5" height="5" fill="black" />
-        <circle cx="185" cy="40" r="3" fill="red" />
 
-        <text
-          x="95"
-          y="45"
-          fontFamily="Segoe UI, Tahoma, Geneva, Verdana, sans-serif"
-          fontSize={24}
-          fill="white"
-          fontWeight="600"
-        >
-          QRcards
-        </text>
+  <text
+    x="150"
+    y="65"
+    fontFamily="Segoe UI, Tahoma, Geneva, Verdana, sans-serif"
+    fontSize="24"
+    fill="white"
+    fontWeight="600"
+    textAnchor="middle"
+  >
+    QRcard
+  </text>
       </svg>
     </>
   );
